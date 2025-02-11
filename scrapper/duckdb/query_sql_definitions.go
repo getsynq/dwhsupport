d7348f1e84d47983e0863762ee71accfbb5ebd63
Lukasz Stefaniak <lustefaniak@gmail.com>
2025-02-11 15:15:04 +0100

    Update support from cloud repo

--- scrapper/duckdb/query_sql_definitions.go
+++ scrapper/duckdb/query_sql_definitions.go
@@ -7,7 +7,7 @@ import (
 	dwhexec "github.com/getsynq/dwhsupport/exec"
 	dwhexecduckdb "github.com/getsynq/dwhsupport/exec/duckdb"
 	"github.com/getsynq/dwhsupport/scrapper"
-	_ "github.com/lib/pq"
+	_ "github.com/marcboeker/go-duckdb"
 )
 
 //go:embed query_sql_definitions.sql
@@ -15,9 +15,8 @@ var querySqlDefinitionsSql string
 
 func (e *DuckDBScrapper) QuerySqlDefinitions(ctx context.Context) ([]*scrapper.SqlDefinitionRow, error) {
 	return dwhexecduckdb.NewQuerier[scrapper.SqlDefinitionRow](e.executor).QueryMany(ctx, querySqlDefinitionsSql,
-		dwhexec.WithArgs[scrapper.SqlDefinitionRow](e.conf.Database),
 		dwhexec.WithPostProcessors(func(row *scrapper.SqlDefinitionRow) (*scrapper.SqlDefinitionRow, error) {
-			row.Database = e.conf.Database
+			row.Instance = e.conf.MotherduckAccount
 			return row, nil
 		}))
 }
