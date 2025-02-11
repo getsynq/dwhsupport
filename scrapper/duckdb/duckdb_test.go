3f7b82272c465aa65cba1cbb48fbafe97991a765
Lukasz Stefaniak <lustefaniak@gmail.com>
2025-02-11 15:15:04 +0100

    Update support from cloud repo

--- scrapper/duckdb/duckdb_test.go
+++ scrapper/duckdb/duckdb_test.go
@@ -21,11 +21,15 @@ func (s *DuckDBScrapperSuite) TestNewDuckDBScrapper() {
 	ctx := context.TODO()
 
 	scrapper, err := NewDuckDBScrapper(ctx, &DuckDBScapperConf{
-		Database:        "synq-pristine",
-		MotherduckToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imx1a2FzekBzeW5xLmlvIiwic2Vzc2lvbiI6Imx1a2Fzei5zeW5xLmlvIiwicGF0IjoiMWxEVWYzWDd5c2I0N0Fua1ItYjE4eVhuRmlQRHpxeHc5R0dCZzZFMTlGWSIsInVzZXJJZCI6IjI2YTU0NWE3LWU2YmEtNGNjYy04ODE2LTVjYTU5OTFhN2Q4ZCIsImlzcyI6Im1kX3BhdCIsInJlYWRPbmx5Ijp0cnVlLCJ0b2tlblR5cGUiOiJyZWFkX3NjYWxpbmciLCJpYXQiOjE3Mzc2NDUyMjh9.e_C7UOusETMriyHtaifUyqoCVscq0yYQ09oa5bwzxNo",
+		MotherduckAccount: "synq-pristine",
+		MotherduckToken:   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imx1a2FzekBzeW5xLmlvIiwic2Vzc2lvbiI6Imx1a2Fzei5zeW5xLmlvIiwicGF0IjoiMWxEVWYzWDd5c2I0N0Fua1ItYjE4eVhuRmlQRHpxeHc5R0dCZzZFMTlGWSIsInVzZXJJZCI6IjI2YTU0NWE3LWU2YmEtNGNjYy04ODE2LTVjYTU5OTFhN2Q4ZCIsImlzcyI6Im1kX3BhdCIsInJlYWRPbmx5Ijp0cnVlLCJ0b2tlblR5cGUiOiJyZWFkX3NjYWxpbmciLCJpYXQiOjE3Mzc2NDUyMjh9.e_C7UOusETMriyHtaifUyqoCVscq0yYQ09oa5bwzxNo",
 	})
 	s.Require().NoError(err)
 	s.Require().NotNil(scrapper)
+
+	databases, err := scrapper.QueryDatabases(ctx)
+	s.Require().NoError(err)
+	s.NotEmpty(databases)
 
 	tables, err := scrapper.QueryTables(ctx)
 	s.Require().NoError(err)
--- scrapper/duckdb/query_table_metrics.go
+++ scrapper/duckdb/query_table_metrics.go
@@ -8,7 +8,7 @@ import (
 	dwhexec "github.com/getsynq/dwhsupport/exec"
 	dwhexecduckdb "github.com/getsynq/dwhsupport/exec/duckdb"
 	"github.com/getsynq/dwhsupport/scrapper"
-	_ "github.com/lib/pq"
+	_ "github.com/marcboeker/go-duckdb"
 )
 
 //go:embed query_table_metrics.sql
@@ -17,7 +17,7 @@ var queryTableMetricsSql string
 func (e *DuckDBScrapper) QueryTableMetrics(ctx context.Context, lastMetricsFetchTime time.Time) ([]*scrapper.TableMetricsRow, error) {
 	return dwhexecduckdb.NewQuerier[scrapper.TableMetricsRow](e.executor).QueryMany(ctx, queryTableMetricsSql,
 		dwhexec.WithPostProcessors(func(row *scrapper.TableMetricsRow) (*scrapper.TableMetricsRow, error) {
-			row.Database = e.conf.Database
+			row.Instance = e.conf.MotherduckAccount
 			return row, nil
 		}))
 }
