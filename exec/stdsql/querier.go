9f745fb8ae8cef69254319343fa876269f819390
Zdenko Nevrala <nevralaz@gmail.com>
2025-04-05 11:38:52 +0200

    Fix closing ssh connections.

--- exec/stdsql/querier.go
+++ exec/stdsql/querier.go
@@ -2,27 +2,50 @@ package stdsql
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/getsynq/dwhsupport/exec"
 	"github.com/getsynq/dwhsupport/exec/querier"
+	"github.com/getsynq/dwhsupport/sshtunnel"
 	"github.com/jmoiron/sqlx"
 )
 
 var _ querier.Querier[interface{}] = (*StdSqlQuerier[interface{}])(nil)
 
-func NewQuerier[T any](conn *sqlx.DB) *StdSqlQuerier[T] {
-	return &StdSqlQuerier[T]{conn: conn}
+type Querier_Opts[T any] func(querier *StdSqlQuerier[T])
+
+func Querier_WithSshTunnelDialer[T any](sshTunnelDialer *sshtunnel.SshTunnelDialer) Querier_Opts[T] {
+	return func(querier *StdSqlQuerier[T]) {
+		querier.sshTunnelDialer = sshTunnelDialer
+	}
+}
+
+func NewQuerier[T any](conn *sqlx.DB, opts ...Querier_Opts[T]) *StdSqlQuerier[T] {
+	querier := &StdSqlQuerier[T]{conn: conn}
+
+	for _, opt := range opts {
+		opt(querier)
+	}
+
+	return querier
 }
 
 type StdSqlQuerier[T any] struct {
-	conn *sqlx.DB
+	conn            *sqlx.DB
+	sshTunnelDialer *sshtunnel.SshTunnelDialer
 }
 
 func (s StdSqlQuerier[T]) QueryMany(ctx context.Context, sql string, opts ...exec.QueryManyOpt[T]) ([]*T, error) {
 	return QueryMany(ctx, s.conn, sql, opts...)
 }
 
-func (s StdSqlQuerier[T]) QueryAndProcessMany(ctx context.Context, sql string, handler func(ctx context.Context, batch []*T) error, opts ...exec.QueryManyOpt[T]) error {
+func (s StdSqlQuerier[T]) QueryAndProcessMany(
+	ctx context.Context,
+	sql string,
+	handler func(ctx context.Context, batch []*T) error,
+	opts ...exec.QueryManyOpt[T],
+) error {
 	return QueryAndProcessMany(ctx, s.conn, sql, handler, opts...)
 }
 
@@ -35,5 +58,18 @@ func (s StdSqlQuerier[T]) Exec(ctx context.Context, sql string) error {
 }
 
 func (s StdSqlQuerier[T]) Close() error {
-	return s.conn.Close()
+	var errs []error
+	if err := s.conn.Close(); err != nil {
+		errs = append(errs, err)
+	}
+
+	if s.sshTunnelDialer != nil {
+		fmt.Println("closing ssh tunnel")
+		if err := s.sshTunnelDialer.Close(); err != nil {
+			fmt.Println("error closing ssh tunnel", err)
+			errs = append(errs, err)
+		}
+	}
+
+	return errors.Join(errs...)
 }
