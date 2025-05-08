f8d2fe9cf3bcef4a6a85d43f440224ad0d7f11ef
Zdenko Nevrala <nevralaz@gmail.com>
2025-05-08 16:17:22 +0200

    Correct comments.

--- scrapper/trino/query_tables.sql
+++ scrapper/trino/query_tables.sql
@@ -16,16 +16,12 @@ WHERE t.table_schema NOT IN ('information_schema', 'sys')
 UNION ALL
 
 SELECT 
-    v.table_catalog as database,
-    v.table_schema as schema,
-    v.table_name as "table",
+    table_catalog as database,
+    table_schema as schema,
+    table_name as "table",
     'VIEW' AS table_type,
-    c.comment as description,
-    true as is_view,
-    false as is_table
-FROM {{catalog}}.information_schema.views v
-LEFT JOIN system.metadata.table_comments c
-  ON v.table_catalog = c.catalog_name
-  AND v.table_schema = c.schema_name
-  AND v.table_name = c.table_name
-WHERE v.table_schema NOT IN ('information_schema', 'sys')
+    null as description,
+    false as is_table,
+    true as is_view
+FROM {{catalog}}.information_schema.views
+WHERE table_schema NOT IN ('information_schema', 'sys')
