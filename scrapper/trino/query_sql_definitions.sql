1451c96d18f041618b963d25c08937fcc16fafbb
Zdenko Nevrala <nevralaz@gmail.com>
2025-05-09 11:45:23 +0200

    Trino - excluded schemas.

--- scrapper/trino/query_sql_definitions.sql
+++ scrapper/trino/query_sql_definitions.sql
@@ -5,7 +5,7 @@ with tables as (
         table_name,
         table_type
     from {{catalog}}.information_schema.tables
-    where table_schema not in ('information_schema', 'sys')
+    where table_schema not in ('information_schema')
 )
 select 
     t.database,
