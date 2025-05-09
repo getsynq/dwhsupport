729cb396951c3f561ad59bf9fd838519fbfdd67c
Zdenko Nevrala <nevralaz@gmail.com>
2025-05-09 09:53:52 +0200

    Trino scrapper - query catalog. (#27)
    * Trino scrapper - query catalog.
    
    * Update test.

--- scrapper/trino/query_catalog_test.go
+++ scrapper/trino/query_catalog_test.go
@@ -10,18 +10,18 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-type QueryTablesSuite struct {
+type QueryCatalogSuite struct {
 	suite.Suite
 }
 
-func TestQueryTablesSuite(t *testing.T) {
+func TestQueryCatalogSuite(t *testing.T) {
 	if os.Getenv("CI") != "" {
 		t.Skip("skipping Trino test in CI environment")
 	}
-	suite.Run(t, new(QueryTablesSuite))
+	suite.Run(t, new(QueryCatalogSuite))
 }
 
-func (s *QueryTablesSuite) TestQueryTables() {
+func (s *QueryCatalogSuite) TestQueryCatalog() {
 	// please run locally to make test pass:
 	// docker run --name trino -d -p 8080:8080 trinodb/trino
 	ctx := context.TODO()
@@ -30,33 +30,33 @@ func (s *QueryTablesSuite) TestQueryTables() {
 		Port:     8080,
 		User:     "trino",
 		Password: "trino",
-		Catalogs: []string{"tpch"},
+		Catalogs: []string{"iceberg"},
 	}
 	scr, err := NewTrinoScrapper(ctx, conf)
 	s.Require().NoError(err)
 	s.Require().NotNil(scr)
 	defer scr.Close()
 
-	rows, err := scr.QueryTables(ctx)
+	rows, err := scr.QueryCatalog(ctx)
 	s.Require().NoError(err)
 	s.Require().NotEmpty(rows)
-	// get unique databases
-	databases := make(map[string]bool)
-	for _, row := range rows {
-		databases[row.Database] = true
-	}
-	s.True(databases["tpch"])
 	spew.Dump(rows)
 
-	// Spot check first row fields
 	row := rows[0]
-	// no description in default data for tables
-	s.Empty(row.Description)
-	// no tags at all
-	s.Empty(row.Tags)
 	s.Equal("localhost", row.Instance)
-	s.Equal("tpch", row.Database)
+	s.Equal("iceberg", row.Database)
 	s.NotEmpty(row.Schema)
 	s.NotEmpty(row.Table)
-	s.NotEmpty(row.TableType)
+	s.NotEmpty(row.Column)
+	s.NotZero(row.Position)
+	s.NotEmpty(row.Type)
+	// table_comment and comment can be nil, just check type
+	s.IsType(new(string), row.TableComment)
+	s.IsType(new(string), row.Comment)
+
+	// not supported
+	s.False(row.IsStructColumn)
+	s.False(row.IsArrayColumn)
+	s.Empty(row.TableTags)
+	s.Empty(row.ColumnTags)
 }
