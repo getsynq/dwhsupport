843ff0e15089674756540952b7fec398985e716d
Zdenko Nevrala <nevralaz@gmail.com>
2025-06-03 14:16:23 +0200

    OrExpr.

--- sqldialect/cond.go
+++ sqldialect/cond.go
@@ -171,3 +171,22 @@ func Gte(a, b Expr) *CompareExpr {
 func Lte(a, b Expr) *CompareExpr {
 	return compare(a, COMPARE_LTE, b)
 }
+
+type OrExpr struct {
+	conds []CondExpr
+}
+
+func Or(conds ...CondExpr) *OrExpr {
+	return &OrExpr{conds: conds}
+}
+
+func (e *OrExpr) ToSql(dialect Dialect) (string, error) {
+	condsSql, err := exprsToSql(e.conds, dialect)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("(%s)", strings.Join(condsSql, " or ")), nil
+}
+
+func (e *OrExpr) IsCondExpr() {}
