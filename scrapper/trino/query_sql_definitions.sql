6edae132b99c54119c669391e729e4319553d06a
Lukasz Stefaniak <lustefaniak@gmail.com>
2025-08-12 14:59:01 +0200

    Disable Trino table comment retrieval and Trino MV support for now
    In some environments, it causes crashes when in a loop as it tries to verify row-level permissions to system tables.

--- scrapper/trino/query_sql_definitions.sql
+++ scrapper/trino/query_sql_definitions.sql
@@ -12,13 +12,8 @@ select
     t.schema,
     t.table_name as "table",
     (t.table_type = 'VIEW' AND v.view_definition IS NOT NULL) as is_view,
-    mv.name is not null as is_materialized_view,
-    coalesce(mv.definition, v.view_definition, '') as sql
+    coalesce(v.view_definition, '') as sql
 from tables t
 left join {{catalog}}.information_schema.views v
     on t.schema = v.table_schema and t.table_name = v.table_name
-LEFT JOIN system.metadata.materialized_views mv
-    ON t.database = mv.catalog_name
-    AND t.schema = mv.schema_name
-    AND t.table_name = mv.name
 order by t.database, t.schema, t.table_name 
