cfbd08322e5b268930c5feed83f41587528f46c0
Zdenko Nevrala <nevralaz@gmail.com>
2026-05-28 16:00:17 +0200

    sqldialect: add ResolveFieldRef to Dialect interface

--- sqldialect/dialect.go
+++ sqldialect/dialect.go
@@ -22,6 +22,7 @@ type Dialect interface {
 	CurrentTimestamp() Expr
 
 	Identifier(string) string
+	ResolveFieldRef(string) string
 	StringLiteral(string) string
 	ToString(Expr) Expr
 	Coalesce(exprs ...Expr) Expr
